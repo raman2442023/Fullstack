d8830bff5e8f3ac47d7d585090864781847576cf
raman2442023 <raman2442023@gmail.com>
2023-05-31 04:45:03 +0530

    if else task in ecelipse

---
+++ First1.java
@@ -0,0 +1,55 @@
+package Raman.Fullstack;
+
+class First 
+{
+
+ public static void main(String[] args) 
+ {
+	 int mi = 210;
+	 int gt = 201;
+	 First ipl = new First();
+	 ipl.play_qualifers( mi, gt);
+	 
+
+ }	
+ private void play_qualifers(int mi,int gt)
+ {
+	if (mi>gt)
+	{
+	int csk =210;
+	mi = 220;
+	if (csk > mi)
+	{
+	 System.out.println("Dohni gets trophy");
+	}
+	else
+	{
+		System.out.println("Rphit gets trophy");
+	}
+	}
+	
+	else
+	{
+		int csk =180;
+		gt=165;
+		if (csk>gt)
+		{
+			System.out.println("Dohni get trophy1");
+			
+		}
+		else
+		{
+			System.out.println("pandya get trophy");		}
+	}
+		
+	
+	 
+ }
+ 
+	 
+ 
+ 
+ 
+ }	 
+	 
+	 
---
+++ Vote.java
@@ -0,0 +1,28 @@
+package Raman.Fullstack;
+
+public class Vote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	} 
+	int age = 18;
+	String relign = "Indian";
+	{
+	if (age>=18)
+	{
+		if (relign == "Indian")
+		{
+			System.out.println("Your Eligble for vote");
+		}
+		else
+			
+		{
+		
+		    
+			System.out.println("Your not eligible for vote");
+		}
+	}
+	}	
+}
+
