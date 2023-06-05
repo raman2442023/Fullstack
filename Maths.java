52a3b5de1cc5083b2a1eab60258dc7fdab8df1d1
raman2442023 <raman2442023@gmail.com>
2023-06-05 22:19:16 +0530

    Math class methods create using while loop

---
+++ Maths.java
@@ -0,0 +1,19 @@
+package home.work;
+
+public class Maths 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Maths md = new Maths();
+		
+		int num = -125;
+		System.out.println(num);
+		num = Math.abs(num);
+		System.out.println(num);
+	}
+
+	
+
+}
