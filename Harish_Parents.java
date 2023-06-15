877ff737a924ee30d9aa8c66f61259db4ea4756e
raman2442023 <raman2442023@gmail.com>
2023-06-15 22:01:13 +0530

    Access other class and methods in Encapsulation

---
+++ Harish_Parents.java
@@ -0,0 +1,43 @@
+package Nammakkal;
+
+public class Harish_Parents {
+		
+	 int door_no=1004;
+	 int age=30;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Harish_Parents hp = new Harish_Parents();
+		hp.hobby();
+		System.out.println(hp.door_no);
+		hp.personal_details();
+		hp.give_invitation();
+		hp.choose_bride();
+	}
+	
+	
+	protected void choose_bride() {
+		// TODO Auto-generated method stub
+		System.out.println("Status Education");
+	}
+
+
+	void hobby()
+	{
+		
+		System.out.println("Going to ARRS theatre");
+	}
+	void personal_details()
+	{
+		
+		System.out.println("Personal details");
+	}
+	protected void give_invitation()
+	{
+		System.out.println("Harish Marriage");
+	}
+	
+	
+	
+	
+
+}
---
+++ Neighbour.java
@@ -0,0 +1,15 @@
+package Nammakkal;
+
+public class Neighbour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Harish_Parents hh = new Harish_Parents();
+		System.out.println(hh.door_no);
+		System.out.println(hh.age);
+		hh.hobby();
+		hh.personal_details();
+		//hh.give_invitation();
+	}
+
+}
