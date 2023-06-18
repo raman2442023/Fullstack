498be048adbbe4bb7166b62d85499b1086e7a6df
raman2442023 <raman2442023@gmail.com>
2023-06-18 21:47:40 +0530

    Abstract in java

---
+++ Government.java
@@ -0,0 +1,20 @@
+package com.org;
+
+public abstract class Government {
+
+	public abstract void raid ();
+	
+	public abstract void collect_document();
+	
+	public void pay_salary()
+	{
+		
+		System.out.println("Pay Salary");
+	}
+	
+		
+		
+
+	
+
+}
