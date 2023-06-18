aa4a54c1e388467191bff6f44cff12825550c746
raman2442023 <raman2442023@gmail.com>
2023-06-18 21:49:43 +0530

    Abstract in java

---
+++ Enforement.java
@@ -0,0 +1,25 @@
+package com.org;
+
+public class Enforement extends Government{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void raid() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void collect_document() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
