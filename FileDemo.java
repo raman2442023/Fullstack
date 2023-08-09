1b9dce109f1d1d53e7c982dd46acbde6cd8b5e44
raman2442023 <raman2442023@gmail.com>
2023-08-09 21:16:37 +0530

    Learning FileIo

---
+++ FileDemo.java
@@ -0,0 +1,47 @@
+package com.org;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileDemo {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		
+		
+		FileDemo fl = new FileDemo();
+		
+		fl.check_file();
+	}
+
+	private void check_file() {
+		// TODO Auto-generated method stub
+		
+		File ff = new File("//home//raman//Desktop//HR");
+		
+		System.out.println(ff.mkdir());
+		
+	
+//			try 
+//			{
+//				ff.createNewFile();
+//			} 
+//			catch (IOException e) 
+//			{
+//			
+//				e.printStackTrace();
+//			}
+	
+		
+		
+		
+				
+	
+			
+			
+		
+	
+
+	}
+	}
