d18662bab8f3a9a5fe2960c3b4bedbf8785d5f63
raman2442023 <raman2442023@gmail.com>
2024-07-16 12:33:25 +0530

    Task Manager

---
+++ TaskManager-2/src/main/java/com/example/taskmanager/TaskManager2Application.java
@@ -0,0 +1,13 @@
+package com.example.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManager2Application.class, args);
+	}
+
+}
